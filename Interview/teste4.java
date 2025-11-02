8cf6ca823bc935ac39d2b57b74f52b1be386d990
Rodrigo Santana <rodrigosantana.dev@gmail.com>
2025-11-02 09:52:35 -0300

    Teste prático 4

---
+++ Interview/teste4.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+class teste4 {
+    public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Digite a palavra: ");
+		String palavra = sc.nextLine();
+		
+		boolean result = Palindromo(palavra);
+		
+		if(result == true){
+			System.out.println("A palavra " + palavra + " é um palindromo.");
+		}
+		else{
+			System.out.println("A palavra " + palavra + " não é um palindromo.");
+		}
+		
+		sc.close();
+    }
+    
+    public static boolean Palindromo(String palavra){
+        char[] letras = palavra.toCharArray();
+		int tamanho = palavra.length();
+		
+		for(int i=0; i<tamanho; i++){
+			if(letras[i] != letras[tamanho - 1 -i]){
+				return false;
+			}
+		}	
+		return true;
+    }
+        
+}
