80346e0a3dbefa15329bbc3602d8d0d5c7ea258c
Rodrigo Santana <rodrigosantana.dev@gmail.com>
2025-11-11 14:36:44 -0300

    Desafio 21 concluido

--- Interview/teste21.java
+++ Interview/teste21.java
@@ -1,4 +1,31 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class teste21 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Digite um número: ");
+        int num = sc.nextInt();
+
+        int[] numeros = new int[num];
+
+        for(int i=0; i<numeros.length; i++){
+            numeros[i] = i+1;
+        }
+
+        analisaMultiplos(numeros);
+    }
+
+    public static void analisaMultiplos(int[] numeros){
+        for(int num : numeros){
+            if(num % 3 == 0 && num % 5 == 0){
+                System.out.println("FizzBuzz");
+            } else if(num % 3 == 0){
+                System.out.println("Fizz");
+            } else if(num % 5 == 0){
+                System.out.println("Buzz");
+            } else {
+                System.out.println(num);
+            }
+        }
+    }
 }
