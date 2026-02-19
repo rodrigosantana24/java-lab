3668ebc46ecf5e236fc206881755555021ade0ea
Rodrigo Santana <rodrigosantana.dev@gmail.com>
2026-02-19 12:45:01 -0300

    Exercicio 27

---
+++ Interview/teste27.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class teste27 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        Map<String, Integer> votes = new LinkedHashMap<>();
+
+        System.out.print("\nEnter file full path: ");
+        String path = sc.nextLine();
+
+        try(BufferedReader br = new BufferedReader(new FileReader(path))){
+            String line = br.readLine();
+
+            while(line != null){
+                System.out.println(line);
+
+                String[] fields = line.split(",");
+                String name = fields[0];
+                int count = Integer.parseInt(fields[1]);
+
+                if (votes.containsKey(name)) {
+                    int votesSoFar = votes.get(name);
+                    votes.put(name, count + votesSoFar);
+                }
+                else {
+                    votes.put(name, count);
+                }
+
+                line = br.readLine();
+            }
+
+        } catch(IOException e){
+            System.out.println("Error: " + e.getMessage());
+        }
+
+        System.out.println("\nMap: ");
+        for(String key : votes.keySet()){
+            System.out.println(key + ", " + votes.get(key));
+        }
+
+    }
+}
