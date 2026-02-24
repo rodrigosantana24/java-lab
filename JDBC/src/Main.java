b8a9d35ab402ac7c5530c1779032bfbabd946a7d
Rodrigo Santana <rodrigosantana.dev@gmail.com>
2026-02-24 13:58:47 -0300

    Criando uma nova conexão utilizando JDBC

---
+++ JDBC/src/Main.java
@@ -0,0 +1,8 @@
+import java.sql.Connection;
+
+public class Main {
+    public static void main(String[] args){
+        Connection conn = DB.getConnection();
+        DB.closeConnection();
+    }
+}
